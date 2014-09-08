4ff82cd7e4d0c6a39b3922d945ef550d444280c2
Leonard Norberg <leonard.norberg@sunstone.se>
2014-09-08 16:43:37 +0200

    Replaced absolute paths with tokens and added script to replace tokens with new paths

---
+++ set_base_dir.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; 
+    then echo "syntax: $0 basedir"
+    exit
+fi
+
+basedir=$1
+echo "Using basedir $basedir"
+basedir="${basedir//\//\\/}"
+
+for file in fedora/server/config/fedora.fcfg fedora/server/config/spring/akubra-llstore.xml fedora/tomcat/conf/Catalina/localhost/fedora.xml
+do
+  echo Replacing in $file
+  cat $file | sed "s/BASE-DIR-REPLACE-TOKEN/${basedir}/" > $file.tmp
+  mv $file.tmp $file
+done

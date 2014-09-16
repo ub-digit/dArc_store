0cca0a086487c44fbfb3c2600c4d7406c255ab37
Leonard Norberg <leonard.norberg@sunstone.se>
2014-09-16 17:25:50 +0200

    Fixed pid in models. Added relationships between objects

--- scripts/createTestData.rb
+++ scripts/createTestData.rb
@@ -11,7 +11,7 @@ def create_data(repo, modelname)
   pid = repo.ingest()
   obj = repo.find(pid)
   model = find_model(repo, modelname)
-  obj.models << model.pid
+  obj.models << model
   obj
 end
 
@@ -35,20 +35,30 @@ add_dc_field(objAuthority,'title','Test Authority')
 # Archive
 objArchive = create_data(repo, 'Archive')
 add_dc_field(objArchive,'title','Test Archive')
+rels = objArchive.relationship("info:fedora/fedora-system:def/relations-external#isDependentOf")
+rels << objAuthority
 
 # Disk
 objDisk = create_data(repo, 'Disk')
 add_dc_field(objDisk,'title','Test Disk')
+rels = objDisk.relationship("info:fedora/fedora-system:def/relations-external#isPartOf")
+rels << objArchive
 
 # DiskImage
 objDiskImage = create_data(repo, 'DiskImage')
 add_dc_field(objDiskImage,'title','Test DiskImage')
+rels = objDiskImage.relationship("info:fedora/fedora-system:def/relations-external#isSubsetOf")
+rels << objDisk
 
 # DigitalDocument
 objDigitalDocument = create_data(repo, 'DigitalDocument')
 add_dc_field(objDigitalDocument,'title','Test DigitalDocument')
+rels = objDigitalDocument.relationship("info:fedora/fedora-system:def/relations-external#isPartOf")
+rels << objDisk
 
 # ContentFile
 objContentFile = create_data(repo, 'ContentFile')
 add_dc_field(objContentFile,'title','Test ContentFile')
+rels = objContentFile.relationship("info:fedora/fedora-system:def/relations-external#isPartOf")
+rels << objDigitalDocument
 
